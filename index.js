1c887cf1d6367f763a7492ce508c43893e721fcb
S.H. Kim <gaviator15@gmail.com>
2021-03-07 22:50:40 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express(); // express()를 이용하여 새로운 express app 생성
 const port = 5000; // 백엔드 서버의 포트 번호. 비어있는 번호 중 아무거나 원하는 번호로 설정하면 됨.
 
 const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -13,21 +16,18 @@ app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb://raykim:raykim@boilerplate-shard-00-00.zrthn.mongodb.net:27017,boilerplate-shard-00-01.zrthn.mongodb.net:27017,boilerplate-shard-00-02.zrthn.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-9ixsys-shard-0&authSource=admin&retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   // root 경로의 페이지('/')에서 "Hello World!"를 출력.
-  res.send("Hello World! Hi!!!!!");
+  res.send("Hello World! 하이!!!!!");
 });
 
 app.post("/register", (req, res) => {
