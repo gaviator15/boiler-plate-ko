9196067cbab40aec3d4359ef1a1eb3143b53373b
S.H. Kim <gaviator15@gmail.com>
2021-03-08 12:41:38 +0900

    #10 Bcrypt 활용 비밀번호 암호화 완료

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require("express"); // express 모듈 가져오기
 const app = express(); // express()를 이용하여 새로운 express app 생성.
 const port = 5000; // 백엔드 서버의 포트 번호. 비어있는 번호 중 아무거나 원하는 번호로 설정하면 됨.
 
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 
 const config = require("./config/key");
 
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
-app.use(bodyParser.json());
+app.use(express.json());
+//app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -27,7 +29,7 @@ mongoose
 
 app.get("/", (req, res) => {
   // root 경로의 페이지('/')에서 "Hello World!"를 출력.
-  res.send("Hello World! 하이!!!!!");
+  res.send("Hello World! 하이!");
 });
 
 app.post("/register", (req, res) => {
